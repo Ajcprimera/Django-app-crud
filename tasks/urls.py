228c0e3289b57b135cff9ce7954349fa844e8cd8
Albert <c.albertj26@gmail.com>
2024-04-24 16:32:53 -0400

    proyecto practico con Django

---
+++ tasks/urls.py
@@ -0,0 +1,15 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup,name='signup'),
+    path('tasks/',views.task, name='tasks'),
+    path('tasks_completed/',views.task_completed, name='task_completed'),
+    path('signout/', views.signout, name= 'signout'),
+    path('signin/', views.singin, name= 'singin'),
+    path('tasks/create/', views.create_task, name= 'create_task'),
+    path('tasks/<int:task_id>/',views.task_detail, name= 'task_detail'),
+    path('tasks/<int:task_id>/complete/',views.complete_task, name= 'complete_task'),
+    path('tasks/<int:task_id>/delete/',views.delete_task, name= 'delete_task')
+]
